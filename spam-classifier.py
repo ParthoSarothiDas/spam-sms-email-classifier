30f42cbe9051445980b2a1095313e5bf51cdb6c2
Partho Sarothi Das <partho52@gmail.com>
2025-06-15 00:40:46 +0600

    Update spam-classifier.py

--- spam-classifier.py
+++ spam-classifier.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import pickle
-from utils import text_transformer
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
+ps = PorterStemmer()
+
+def text_transformer(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    alnum = []
+    for i in text:
+        if i.isalnum():
+            alnum.append(i)
+            
+    alnum_stop = []
+    for i in alnum:
+        if i not in stopwords.words('english'):
+            alnum_stop.append(i)
+
+    final_text = []
+    for i in alnum_stop:
+        final_text.append(ps.stem(i))
+    return " ".join(final_text)
 
 @st.cache_resource
 def load_model():
