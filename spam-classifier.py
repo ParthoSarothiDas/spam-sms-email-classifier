25459670b4dfad0d43a4a1203662078dd717e7c4
Partho Sarothi Das <partho52@gmail.com>
2025-06-15 00:09:58 +0600

    Add files via upload

---
+++ spam-classifier.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pickle
+from utils import text_transformer
+
+@st.cache_resource
+def load_model():
+    with open('data/mnb_model.pkl', 'rb') as file:
+        mnb = pickle.load(file)
+    return mnb
+
+@ st.cache_resource
+def load_tfidf():
+    with open('data/tfidf.pkl', 'rb') as file:
+        tfidf = pickle.load(file)
+    return tfidf
+
+mnb = load_model()
+tfidf = load_tfidf()
+
+
+st.title('Spam SMS/Email Classifier')
+text = st.text_area('Input your SMS or email for classify:')
+clicked = st.button('Classify')
+if clicked:
+    if text == '':
+        st.error('Please input sms or email for classify.')
+    else:
+        transformed_text = text_transformer(text)
+        vectorized = tfidf.transform([transformed_text])
+        result = mnb.predict(vectorized)[0]
+
+        if result == 0:
+            st.info('This is not a spam')
+        else:
+            st.error('This is a SPAM.')
+
+
+# Footer
+st.markdown("<br><br>", unsafe_allow_html=True)
+st.markdown("""
+<hr style="margin-top: 30px;">
+<div style="text-align: center; font-size: 0.9em; color: gray;">
+    Created by <b>Partho Sarothi Das</b><br>
+    <i>Aspiring Data Scientist | Passionate about ML & Visualization</i><br>
+    Email: <a href="mailto:partho52@gmail.com">partho52@gmail.com</a>
+</div>
+""", unsafe_allow_html=True)
